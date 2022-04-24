94c5d353a870b00d8eafbf31d53e42e901fd7bff
Mukesh Otwani <mukeshotwani.50@gmail.com>
2022-04-24 10:06:52 +0530

    added new compiler plugin and added new parameter to execute test from cmd

---
+++ src/test/java/com/learnautomation/testcases/Sample.java
@@ -0,0 +1,12 @@
+package com.learnautomation.testcases;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void helloTest() {
+		System.out.println("Sample test executed");
+	}
+
+}
